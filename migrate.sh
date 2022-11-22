9c0b8d6f067b42abe177ede8a71fbc983e1af3c6
CrimsonTome <crimsontome427@protonmail.com>
2022-11-22 15:23:21 +0000

    chore: add package list wip

--- migrate.sh
+++ migrate.sh
@@ -16,4 +16,4 @@ gh repo list --limit 100 |awk '{print $1}'  | xargs -L1 gh repo clone
 # cloning repos in my stack, credit to @Epictek for the oneliner. html-xml-utils isnt maintained on fedora so we use a docker container. This is slower but should ensure it works on any distribution
 
 # TODO fix so it loops through all pages, as it just grabs the first currently
-curl -s https://github.com/stars/CrimsonTome/lists/my-stack | docker run -i jezzay/html-xml-utils hxnormalize -x |docker run -i jezzay/html-xml-utils hxselect -c '#user-list-repositories > div > div > h3' | sed -n 's/.*href="\([^"]*\).*/\1/p' | awk '{print "https://github.com"$0}'|xargs -L1 git clone
+# curl -s https://github.com/stars/CrimsonTome/lists/my-stack | docker run -i jezzay/html-xml-utils hxnormalize -x |docker run -i jezzay/html-xml-utils hxselect -c '#user-list-repositories > div > div > h3' | sed -n 's/.*href="\([^"]*\).*/\1/p' | awk '{print "https://github.com"$0}'|xargs -L1 git clone
